b175e144388c3e1d3a9cdfd31b24107e27735365
Maicfer <mc.maicfer@gmail.com>
2025-05-12 20:31:14 -0500

    cambios .env, package, api.js, vite

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,9 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://namitask.onrender.com/api/', // ✅ URL fija de producción
 });
 
 export default api;
+
