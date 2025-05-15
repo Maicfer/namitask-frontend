b46bf60c353018179fabe308a14fe4a7063cce68
Maicfer <mc.maicfer@gmail.com>
2025-05-15 15:50:19 -0500

    cambios varios

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,8 +8,9 @@ const Dashboard = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="bg-dashboard-pattern bg-cover bg-center bg-fixed min-h-screen h-full pt-16 p-8">
+    <div className="min-h-screen h-full pt-16 p-8"> {/* Eliminamos las clases de fondo aquí */}
       <div className="max-w-xl mx-auto bg-white rounded-xl p-6 shadow-lg text-center space-y-4">
+        {/* Los botones vuelven aquí */}
         <div className="flex justify-center gap-4 mt-4 flex-wrap">
           <button
             onClick={() => navigate("/profile")}
