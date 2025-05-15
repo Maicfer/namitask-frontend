14db6552731355e5ceded73d453c4a3fed41f9c8
Maicfer <mc.maicfer@gmail.com>
2025-05-15 16:54:55 -0500

    cambios varios

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,18 +6,19 @@ import "../index.css";
 const Dashboard = () => {
   const { logoutUser } = useContext(AuthContext);
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext); // Asegúrate de que estamos importando 'user'
 
   return (
     <div className="min-h-screen h-full pt-16 p-8">
       <div className="max-w-xl mx-auto bg-white rounded-xl p-6 shadow-lg text-center space-y-4">
         <div className="text-center">
           <h1 className="text-2xl font-bold text-indigo-700">
-            Bienvenido, {user?.nombre_completo || "Usuario"}
+            Bienvenido, {user ? user.nombre_completo || "Usuario" : "Cargando..."}
           </h1>
-          <p className="text-gray-600">{user?.email}</p>
+          <p className="text-gray-600">{user ? user.email : "Cargando..."}</p>
         </div>
         {/* Contenedor de botones */}
-        <div className="flex justify-center gap-4 mt-8 flex-wrap"> {/* Aumentamos el margen superior */}
+        <div className="flex justify-center gap-4 mt-8 flex-wrap">
           <button
             onClick={() => navigate("/profile")}
             className="button-primary"
