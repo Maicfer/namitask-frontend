1e7a8cc0a7fb48e64d970c4135c90e9d981b9852
Maicfer <mc.maicfer@gmail.com>
2025-05-15 15:27:29 -0500

    cambios 1

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,21 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
-import "../index.css"; // Importa los estilos generales
+import "../../index.css"; // Importa los estilos generales
 
 const Dashboard = () => {
   const { user, logoutUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen bg-dashboard-pattern bg-cover bg-center bg-fixed pt-16 p-8">
+    <div className="bg-dashboard-pattern bg-cover bg-center bg-fixed min-h-screen pt-16 p-8"> {/* Aplicamos el fondo aquí */}
       <div className="max-w-xl mx-auto bg-white rounded-xl p-6 shadow-lg text-center space-y-4">
-        <h1 className="text-2xl font-bold text-indigo-700">
-          Bienvenido, {user?.nombre_completo || "Usuario"}
-        </h1>
-        <p className="text-gray-600">Correo: {user?.email}</p>
+        <div className="text-center"> {/* Contenedor para Bienvenido y Correo */}
+          <h1 className="text-2xl font-bold text-indigo-700">
+            Bienvenido, {user?.nombre_completo || "Usuario"}
+          </h1>
+          <p className="text-gray-600">{user?.email}</p>
+        </div>
 
         <div className="flex justify-center gap-4 mt-4 flex-wrap">
           <button
