b700a125dcad45d76066876dd2047a6b7be51420
Maicfer <mc.maicfer@gmail.com>
2025-05-15 14:50:21 -0500

    cambios navbar logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: [
       "./index.html",
       "./src/**/*.{js,ts,jsx,tsx}",
+      "./node_modules/bootstrap/dist/css/bootstrap.min.css"
     ],
     theme: {
       extend: {},
