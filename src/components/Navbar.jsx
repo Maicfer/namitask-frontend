3c2a26bedb0af62fa5ca93a8207a26b8656e502b
Maicfer <mc.maicfer@gmail.com>
2025-05-15 14:38:50 -0500

    cambios navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,24 @@
-import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="flex items-center px-6 py-4 bg-white shadow-md fixed top-0 left-0 right-0 z-50">
+    <nav className="navbar-floating">
       <div
-        className="flex items-center cursor-pointer"
+        className="logo-container"
         onClick={() => navigate("/dashboard")}
       >
         <img
-          src="/logo-temporal.png" // Reemplaza con la ruta correcta de tu logo
+          src="/logo-temporal.png"
           alt="Logo"
-          className="h-8 w-8 mr-2"
+          className="logo"
         />
-        <span className="text-xl font-semibold text-indigo-700">NamiTask</span>
+        <span className="logo-text">NamiTask</span>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-
