e704c3f6ed7685f7b0f15fbbed6f44847b4f8f57
Maicfer <mc.maicfer@gmail.com>
2025-05-15 15:45:16 -0500

    cambios varios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext"; // Importa el contexto de autenticación
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext); // Obtén la información del usuario del contexto
 
   return (
     <nav className="navbar-floating">
@@ -17,6 +19,12 @@ const Navbar = () => {
         />
         <span className="logo-text">NamiTask</span>
       </div>
+      {user && (
+        <div className="user-info">
+          <span className="welcome-text">Bienvenido, {user.nombre_completo || "Usuario"}</span>
+          <span className="email-text">{user.email}</span>
+        </div>
+      )}
     </nav>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,19 +4,12 @@ import { useNavigate } from "react-router-dom";
 import "../index.css";
 
 const Dashboard = () => {
-  const { user, logoutUser } = useContext(AuthContext);
+  const { logoutUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
   return (
-    <div className="bg-dashboard-pattern bg-cover bg-center bg-fixed min-h-screen h-full pt-16 p-8"> {/* Añadimos h-full */}
+    <div className="bg-dashboard-pattern bg-cover bg-center bg-fixed min-h-screen h-full pt-16 p-8">
       <div className="max-w-xl mx-auto bg-white rounded-xl p-6 shadow-lg text-center space-y-4">
-        <div className="text-center">
-          <h1 className="text-2xl font-bold text-indigo-700">
-            Bienvenido, {user?.nombre_completo || "Usuario"}
-          </h1>
-          <p className="text-gray-600">{user?.email}</p>
-        </div>
-
         <div className="flex justify-center gap-4 mt-4 flex-wrap">
           <button
             onClick={() => navigate("/profile")}
